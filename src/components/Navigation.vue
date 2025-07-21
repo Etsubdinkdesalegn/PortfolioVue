07f28ca3b247625fc547be54a1711b2e7ed05a72
Etsubdink <etsubdinkdesalegnadamu@gmail.com>
2025-07-21 13:44:37 -0700

    secondary commit

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,7 +4,7 @@
       <div class="text-2xl font-bold text-green-600 dark:text-green-400">
         <img :src="logo" alt="My Portfolio" class="w-10 h-10 rounded-full border-2 border-green-500">
       </div>
-    <!-- Desktop Menu -->
+ 
       <ul class="hidden md:flex space-x-6 text-gray-700 dark:text-gray-300">
         <li><router-link to="/" class="hover:text-green-500">Home</router-link></li>
         <li><router-link to="/about" class="hover:text-green-500">About</router-link></li>
@@ -12,7 +12,7 @@
         <li><router-link to="/projects" class="hover:text-green-500">Projects</router-link></li>
         <li><router-link to="/contact" class="hover:text-green-500">Contact</router-link></li>
       </ul>
-       <!-- Mobile Toggle Button -->
+     
       <button @click="toggleMenu" class="md:hidden focus:outline-none">
         <svg v-if="!menuOpen" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -24,7 +24,7 @@
         </svg>
       </button>
     </div>
-    <!-- Mobile Menu -->
+   
     <div v-if="menuOpen" class="md:hidden px-6 pb-4 space-y-2 text-gray-700 dark:text-gray-300">
       <router-link @click="closeMenu" to="/" class="block py-2 hover:text-green-500">Home</router-link>
       <router-link @click="closeMenu" to="/about" class="block py-2 hover:text-green-500">About</router-link>
